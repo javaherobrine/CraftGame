e2ad1303fdfdea7a95a9ef53b8436c9825682133
javaherobrine <620009897@qq.com>
2020-10-15 12:32:32 +0800

    preparing rebuilt network framework

--- io/github/javaherobrine/net/OutputThread.java
+++ io/github/javaherobrine/net/OutputThread.java
@@ -2,93 +2,12 @@ package io.github.javaherobrine.net;
 import java.io.*;
 import java.net.*;
 import io.github.javaherobrine.ioStream.*;
-import java.util.zip.*;
-/**
- * 输出线程
- * @author Java_Herobrine
- */
 public class OutputThread extends Thread implements Closeable,AutoCloseable{
-	BufferedOutputStream os;
-	private ObjectOutputStream thisOos;
-	Socket soc=null;
-	public TCPOutputStream tos;
-	public volatile boolean canWrite=true;
-	public boolean flag=true;
-	public volatile byte[] outputData=null;
-	public void run() {
-		while(flag) {
-			if(outputData!=null) {
-				try {
-					os.write(IOUtils.intToByte4(outputData.length));
-					os.flush();
-					os.write(outputData);
-					os.flush();
-					outputData=null;
-					canWrite=true;
-				}catch(SocketException e) {
-					break;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}else {
-				try {
-					sleep(Long.MAX_VALUE);
-				} catch (InterruptedException e) {
-				}
-			}
-		}
-	}
-	public InputThread getInput() throws IOException {
-		if(soc!=null) {
-			return new InputThread(soc);
-		}
-		return null;
-	}
-	public void init() throws IOException {
-		thisOos=new ObjectOutputStream(os);
-	}
-	/**
-	 * 根据指定的套接字的输出流创建输出线程
-	 * @param soc 套接字
-	 * @throws IOException 如果getOutputStream出错
-	 */
-	public OutputThread(Socket soc) throws IOException {
-		this(soc.getOutputStream());
-	}
-	/**
-	 * 根据指定的输出流创建线程
-	 * @param os 输出流
-	 * @throws IOException 
-	 */
-	public OutputThread(OutputStream os) throws IOException {
-		this.os=new BufferedOutputStream(os);
-		tos=new TCPOutputStream(os);
-	}
-	public void object(Object obj) throws IOException {
-		thisOos.writeObject(obj);
-	}
-	/**
-	 * 向该线程的输出流按照预设的TCP格式写数据
-	 * @param data 写的数据
-	 * @throws IOException 
-	 */
-	public synchronized void write(byte[] data) throws IOException {
-		while(!canWrite) {}
-		canWrite=false;
-		outputData=data;
-		interrupt();
-	}
-	/**
-	 * 直接写数据
-	 * @param data 写的数据
-	 * @throws IOException 如果写入错误
-	 */
-	public synchronized void write0(byte[] data) throws IOException {
-		os.write(data);
-		os.flush();
-	}
+	Socket soc;
 	@Override
 	public void close() throws IOException {
-		os.close();
+	}
+	public OutputThread(Socket soc) {
+		this.soc=soc;
 	}
 }
--- io/github/javaherobrine/net/Server.java
+++ io/github/javaherobrine/net/Server.java
@@ -3,65 +3,8 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 public class Server implements Closeable {
-	public static HashMap<String,Socket> sockets=new HashMap<>();
-	public int serverPort;
-	public ServerSocket s;
-	public ArrayList<Client> clients=new ArrayList<>();
-	public void open() throws IOException {
-		this.s = new ServerSocket(this.serverPort);
-	}
-	public Server(int port) {
-		this.serverPort = port;
-	}
+	@Override
 	public void close() throws IOException {
-		this.s.close();
-	}
-	public static void disconnection(String playerName) throws IOException {
-		sockets.get(playerName).close();
-	}
-	public Client accept() throws IOException {
-		return shakeHands(Client.initClientFromSocket(s.accept()));
-	}
-	public static Client shakeHands(Client c,int id) throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(c.tis,"UTF-8"));
-		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(c.tos,"UTF-8"));
-		DataOutputStream dos=new DataOutputStream(c.tos);
-		TransmissionFormat[] allFormats=TransmissionFormat.values();
-		while(true) {
-			try {
-				TransmissionFormat f=TransmissionFormat.valueOf(br.readLine());
-				if(f==TransmissionFormat.FINISH) {
-					c.msg.connected=false;
-				}
-				if(Arrays.binarySearch(allFormats, f)>0) {
-					c.msg.format=f;
-					bw.write(TransmissionStatus.ACCEPTED.toString());
-					bw.newLine();
-					bw.flush();
-					break;
-				}else {
-					bw.write(TransmissionStatus.CONTINUE.toString());
-					bw.newLine();
-					bw.flush();
-				}
-			}catch(Exception e) {}
-		}
-		c.msg.id=id;
-		dos.writeInt(id);
-		return c;
-	}
-	public Client shakeHands(Client c) throws IOException {
-		DataInputStream dis=new DataInputStream(c.tis);
-		int id=dis.readInt();
-		if(id!=-1) {
-			Client temp=clients.get(id);
-			c.msg=temp.msg;
-			c.msg.connected=true;
-			clients.remove(id);
-			clients.add(id,c);
-			return c;
-		}
-		clients.add(c);
-		return shakeHands(c,clients.indexOf(c));
+		
 	}
 }
