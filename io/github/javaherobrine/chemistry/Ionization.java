ab06158e18f95d56f7366ec6b02b6a12a702e184
javaherobrine <620009897@qq.com>
2020-10-25 21:27:18 +0800

    preparing build chemistry engine

---
+++ io/github/javaherobrine/chemistry/Ionization.java
@@ -0,0 +1,8 @@
+package io.github.javaherobrine.chemistry;
+public abstract class Ionization {
+	Compound electrolyte;
+	public Ionization(Compound electrolyte) {
+		this.electrolyte=electrolyte;
+	}
+	public abstract Ions[] electrolysis(Compound comp);
+}
