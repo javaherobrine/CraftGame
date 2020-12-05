f0936b91cadaf6d96f57c64c4dfa63969f17c8c8
javaherobrine <620009897@qq.com>
2020-12-05 08:31:49 +0800

    add 2 methods

--- io/github/javaherobrine/net/InputThread.java
+++ io/github/javaherobrine/net/InputThread.java
@@ -2,12 +2,10 @@ package io.github.javaherobrine.net;
 import java.io.*;
 public class InputThread extends Thread implements Closeable,AutoCloseable{
 	TCPInputStream in;
+	boolean keep=true;
 	private byte[] now=null;
 	boolean live=true;
 	@Override
-	/**
-	 * 结束线程并关闭流
-	 */
 	public void close() throws IOException {
 		in.close();
 		live=false;
@@ -15,34 +13,35 @@ public class InputThread extends Thread implements Closeable,AutoCloseable{
 	@Override
 	public void run() {
 		while(live) {
-			try {
-				sleep(Long.MAX_VALUE);
-			} catch (InterruptedException e) {
+			if(keep) {
 				try {
-					synchronized(now) {
-						now=in.readData();
-					}
-				} catch (IOException ee) {
+					sleep(Long.MAX_VALUE);
+				} catch (InterruptedException e) {}
+			}
+			try {
+				synchronized(now) {
+					now=in.readData();
 				}
+			} catch (IOException ee) {
 			}
 		}
 	}
-	/**
-	 * 命令输入线程开始一次输入操作
-	 */
 	public void ready() {
 		interrupt();
 	}
 	public InputThread(TCPInputStream in) {
 		this.in=in;
 	}
-	/**
-	 * 读取数据的时候会给数据加锁，此操作会在读取数据之后返回（也有加锁）
-	 * @return 数据
-	 */
-	public byte[] get() {
+	public synchronized byte[] get() {
 		synchronized(now){
 			return now;
 		}
+	}
+	public synchronized void keepTransmitting() {
+		keep=false;
+		ready();
+	}
+	public synchronized void blockTransmitting() {
+		keep=true;
 	}
 }
