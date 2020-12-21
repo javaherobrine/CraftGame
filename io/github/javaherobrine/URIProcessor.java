bf5f18b2bcdade9f1fbb5fbee2699266498971a7
javaherobrine <620009897@qq.com>
2020-12-21 12:58:15 +0800

    SMTP protocol

--- io/github/javaherobrine/URIProcessor.java
+++ io/github/javaherobrine/URIProcessor.java
@@ -2,7 +2,9 @@ package io.github.javaherobrine;
 import java.awt.*;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 public final class URIProcessor {
+	public static final Base64.Encoder ENCODER=Base64.getEncoder();
 	public static final Desktop UTIL=Desktop.getDesktop();
 	private URIProcessor() {}
 	public static final boolean isURL(URI uri) {
@@ -17,6 +19,38 @@ public final class URIProcessor {
 		UTIL.browse(uri);
 	}
 	public static final void mailto(String addr) throws IOException {
-		UTIL.mail(URI.create("mailto:"+addr));
+		try {
+			UTIL.mail(URI.create("mailto:"+addr));
+		}catch(UnsupportedOperationException e) {
+			
+		}
+	}
+	public static final void sendMailWithSMTP(String username,String password,String subject,String content,String from,String to) throws IOException{
+		Socket soc=new Socket("smtp.qq.com",25);
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+		bw.write("HELO jaro");
+		bw.newLine();
+		bw.write("AUTH LOGIN");
+		bw.newLine();
+		bw.write(ENCODER.encodeToString(username.getBytes()));
+		bw.newLine();
+		bw.write(ENCODER.encodeToString(password.getBytes()));
+		bw.newLine();
+		bw.write("DATA");
+		bw.newLine();
+		bw.write("MAIL FROM:"+from);
+		bw.newLine();
+		bw.write("RCPT TO:"+to);
+		bw.newLine();
+		bw.write("SUBJECT:"+subject);
+		bw.newLine();
+		bw.newLine();
+		bw.write(content);
+		bw.newLine();
+		bw.write(".");
+		bw.newLine();
+		bw.write("QUIT");
+		bw.flush();
+		soc.close();
 	}
 }
