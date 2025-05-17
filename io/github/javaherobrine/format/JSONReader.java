b9891df4c23eab97140f5b1fe3272c9ac6064f1c
javaherobrine <620009897@qq.com>
2025-05-17 22:30:03 +0800

    [I/O] JSON Input

--- io/github/javaherobrine/format/JSONReader.java
+++ io/github/javaherobrine/format/JSONReader.java
@@ -11,29 +11,55 @@ public class JSONReader extends FilterReader {
 		this(new BufferedReader(new InputStreamReader(in)));
 	}
 	@Override
-	public int read() throws IOException {
+	public synchronized int read() throws IOException {
 		while(unread!=-1&&Character.isWhitespace(unread)) {
 			unread=in.read();
 		}
 		int ch=unread;
+		System.err.println("read character: "+(char) ch);
 		unread=' ';
 		return ch;
 	}
-	public String nextString() throws IOException{
+	public synchronized Object readObject() throws IOException{
+		char ch=(char) read();
+		Object value=null;
+		if(Character.isDigit(ch)||ch=='.'||ch=='+'||ch=='-') {
+			value=nextNumber(ch);
+		}else if(ch=='{') {
+			value=nextObject();
+		}else if(ch=='[') {
+			value=nextArray();
+		}else if(ch=='t') {
+			in.skip(3);
+			value=true;
+		}else if(ch=='f') {
+			in.skip(4);
+			value=false;
+		}else if(ch=='n') {
+			value=null;
+			in.skip(3);
+		}else if(ch=='\''||ch=='\"') {
+			value=nextString(ch);
+		}
+		return value;
+	}
+	public synchronized String nextString(char end) throws IOException{
+		System.err.println("end="+end);
 		StringBuilder sb=new StringBuilder();
-		boolean n=true;
+		boolean n=false;
 		char ch=(char)in.read();
-		while(ch!='\"'&&n) {
-			n=true;
+		while(ch!=end||n) {
+			System.err.println("current character: "+ch);
+			n=false;
 			if(ch=='\\') {
-				n=false;
+				n=true;
 			}
 			sb.append(ch);
 			ch=(char)in.read();
 		}
 		return sb.toString().translateEscapes();
 	}
-	public Number nextNumber(char first) throws IOException {
+	public synchronized Number nextNumber(char first) throws IOException {
 		int factor=1;
 		if(first=='-'){
 			factor=-1;
@@ -51,6 +77,7 @@ public class JSONReader extends FilterReader {
 			boolean decimal=false;
 			StringBuilder input=new StringBuilder();
 			while(Character.isDigit(first)||first=='E'||first=='+'||first=='-'||first=='.') {
+				System.err.println("current character: "+first);
 				if(first=='E'||first=='.') {
 					decimal=true;
 				}
@@ -65,14 +92,19 @@ public class JSONReader extends FilterReader {
 			}
 		}
 	}
-	public Object[] nextArray() throws IOException{
+	public synchronized Object[] nextArray() throws IOException{
 		LinkedList<Object> arr=new LinkedList<>();
-		char ch=(char)read();
-		while(ch!=']') {
+		char ch=' ';
+		while(true) {
 			if(ch==',') {
+				ch=(char)read();
 				continue;
 			}
 			ch=(char) read();
+			if(ch==']') {
+				break;
+			}
+			System.err.println("current character: "+ch);
 			Object value=null;
 			if(Character.isDigit(ch)||ch=='.'||ch=='+'||ch=='-') {
 				value=nextNumber(ch);
@@ -89,20 +121,24 @@ public class JSONReader extends FilterReader {
 			}else if(ch=='n') {
 				value=null;
 				in.skip(3);
+			}else if(ch=='\''||ch=='\"') {
+				System.err.println("String");
+				value=nextString(ch);
 			}
 			arr.add(value);
-			ch=(char) read();
 		}
 		return arr.toArray();
 	}
-	public HashMap<String,Object> nextObject() throws IOException{
+	public synchronized HashMap<String,Object> nextObject() throws IOException{
 		HashMap<String,Object> res=new HashMap<>();
 		char ch=(char)read();
 		while(ch!='}') {
+			System.err.println("current character: "+ch);
 			if(ch==',') {
+				ch=(char)read();
 				continue;
 			}
-			String str=nextString();//key
+			String str=nextString(ch);//key
 			read();//:
 			ch=(char) read();
 			Object value=null;
@@ -121,6 +157,8 @@ public class JSONReader extends FilterReader {
 			}else if(ch=='n') {
 				value=null;
 				in.skip(3);
+			}else if(ch=='\''||ch=='\"') {
+				value=nextString(ch);
 			}
 			res.put(str, value);
 			ch=(char) read();
